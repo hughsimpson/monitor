0cff7568dd90d46d36c1e6453aaf8f5ef1dcba9c
Hugh Simpson <hughrsimpson@gmail.com>
2013-11-15 15:14:18 +0000

    Rebasing from upstream

--- agent-akka/src/test/scala/org/eigengo/monitor/agent/akka/ActorCountFilteringSpec.scala
+++ agent-akka/src/test/scala/org/eigengo/monitor/agent/akka/ActorCountFilteringSpec.scala
@@ -11,8 +11,11 @@ class ActorCountFilteringSpec extends ActorCellMonitoringAspectSpec(Some("count-
 
     // records the count of actors, grouped by simple class name
     "Not record the count of exluded actors" in {
-      val monitoredActorTag = "org.eigengo.monitor.agent.akka.SimpleActor"
-      val unmonitoredActorTag = "org.eigengo.monitor.agent.akka.KillableActor"
+      val parent = "parent.akka://default/user"
+      val path = "path.akka://default/user/simple"
+      val props = "props.class org.eigengo.monitor.agent.akka.SimpleActor"
+      val className = "className.org.eigengo.monitor.agent.akka.SimpleActor"
+      val tags = List(parent, path, props, className)
       TestCounterInterface.clear()
 
       val monitoredActor = system.actorOf(Props[SimpleActor], "simple")
@@ -24,7 +27,7 @@ class ActorCountFilteringSpec extends ActorCellMonitoringAspectSpec(Some("count-
 
       val counterBeforeKill = TestCounterInterface.foldlByAspect(actorCount)((a,_) =>a)
       counterBeforeKill.size === 1
-      counterBeforeKill must contain(TestCounter(actorCount, 1, List(monitoredActorTag)))
+      counterBeforeKill must contain(TestCounter(actorCount, 1, tags))
 
       monitoredActor ! 'stop
       unmonitoredActor ! 'stop
@@ -33,7 +36,7 @@ class ActorCountFilteringSpec extends ActorCellMonitoringAspectSpec(Some("count-
 
       val counterAfterKill = TestCounterInterface.foldlByAspect(actorCount)((a,_) =>a)
       counterAfterKill.size === 2
-      counterAfterKill must contain(TestCounter(actorCount, 0, List(monitoredActorTag)))
+      counterAfterKill must contain(TestCounter(actorCount, 0, tags))
     }
   }
 }
